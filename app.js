fcc1e32d257ba4617c5671c3eea912a96d51021d
test person <test@test.net>
2023-08-19 18:21:30 +0530

    Implement Error 404 generic message

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 
 
 // middleware
@@ -11,6 +12,8 @@ app.use(express.json())
 
 // routes
 app.use('/api/v1/tasks',tasks)
+
+app.use(notFound)
 
 
 const port = 3000
